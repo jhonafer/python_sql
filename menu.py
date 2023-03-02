0fc503127b08a9895cd6fa5e97aed03bfaa561e3
jhonatan <rizzijhonatan17@gmail.com>
2023-03-02 03:25:42 -0300

    atualizado opcoes

--- menu.py
+++ menu.py
@@ -1,4 +1,6 @@
 from config import conexao
+from opt01 import sql
+
 cursor = conexao.cursor()
 
 print("selecione a consulta")
@@ -13,8 +15,10 @@ print("08 - selecionar todas as pessoas ativas exibindo todas as colunas ordenan
 print("09 - selecionar todas as cidades que nao possuem pessoas")
 print("10 - selecionar quais pessoas tem e-mail do hotmail")
 
+opt=0
 opt=int(input(print("Qual opção: ")))
-sql: str
+
+
 
 match opt:
     case 1:
@@ -24,28 +28,54 @@ match opt:
         sql="select * from cidade"
         
     case 3:
-        sql = ""
+        sql="""SELECT cidade.UF, COUNT(cidade.id) AS quantidade_de_cidades
+        FROM cidade
+        GROUP BY cidade.UF;
+        """
       
     case 4:
-        sql = ""
-       
+        sql="""SELECT *
+        FROM pessoa
+        WHERE nascimento IS NOT NULL
+        ORDER BY nascimento asc
+        limit 1;"""
+        
     case 5:
-        sql = ""
+        sql="""SELECT *
+        FROM pessoa
+        WHERE nascimento IS NOT NULL
+        ORDER BY nascimento desc
+        limit 1;"""
        
     case 6:
-        sql = ""
+        sql="""select p.id, p.nome, p.endereco, p.telefone, p.celular, p.cidade, p.nascimento
+                from pessoa p 
+                left outer join cidade c 
+                on p.cidade = c.Id 
+                where c.Uf = 'PR'"""
      
     case 7:
-        sql = ""
+        sql="""SELECT c.nome AS cidade, COUNT(p.id) AS quantidade_de_pessoas
+        FROM cidade c
+        INNER JOIN pessoa p ON c.id = p.cidade
+        GROUP BY c.nome"""
+
       
     case 8:
-        sql = ""
+        sql="""select * from pessoa
+                where ativo = true
+                order by telefone"""
         
     case 9:
-        sql = ""
+        sql="""select c.Id, c.Nome, c.Uf
+                from cidade c
+                left outer join pessoa p
+                on p.cidade = c.id
+                where p.id is null"""
         
     case 10:
-        sql = ""
+        sql="""select * from pessoa
+                where email like '%@hotmail.com'"""
    
 cursor.execute(sql)
 
---
+++ opt01.py
@@ -0,0 +1,3 @@
+
+
+sql="select * from pessoa where nascimento <= DATE_SUB(CURRENT_DATE, INTERVAL 18 YEAR)"
